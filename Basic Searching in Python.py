a4639b82cba25dd1d27b8ba4bb563288f5044c75
MH-SHUVO20 <mdmehedihasanshuvo994@gmail.com>
2025-09-24 12:33:28 +0600

    Add Basic Searching in Python.py - demonstrate searching operations in lists

---
+++ Basic Searching in Python.py
@@ -0,0 +1,26 @@
+name=["MEHEDI","HASAN","SHUVO"]#List of string
+#searching in list
+print("MEHEDI" in name)
+print("RAHIM" in name)
+print("S" not in name)
+print("R" not in name)
+print("H"  in name)
+#list of numeric
+numeric=[1,2,3,4,5,6,7,8,9]
+#searching in numeric list
+print(5 in numeric)
+print(15 in numeric)
+print(123 not in numeric)
+print(9 not in numeric)
+print(1 in numeric) 
+print(0 in numeric)
+print(10 not in numeric)
+#list of char
+char=['a','b','c','d','e','f','g']
+#searching in char list
+print('a' in char)
+print('z' in char)
+print('m' not in char)
+print('g' not in char)
+print('d' in char)
+print('x' in char)
