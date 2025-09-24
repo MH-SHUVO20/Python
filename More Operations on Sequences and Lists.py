ed8895887ce67bfa8f35a4f8d2df98c9ad187119
MH-SHUVO20 <mdmehedihasanshuvo994@gmail.com>
2025-09-24 12:49:49 +0600

    Add More Operations on Sequences and Lists.py - advanced sequence manipulation examples

---
+++ More Operations on Sequences and Lists.py
@@ -0,0 +1,23 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+list = ["MEHEDI", "HASAN", "SHUVO"]
+print(list)
+print(len(list))
+#change list item
+list[0] = "MEHEDI HASAN"
+print(list)
+#append item to list
+list.append("RAHAT")
+print(list)
+#change in number list
+numbers[0] = 10
+print(numbers)
+#append item to number list
+numbers.append(11)
+print(numbers)
+#insert item to list
+numbers.insert(0, 0)
+print(numbers)
