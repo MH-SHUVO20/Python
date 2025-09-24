058c928b0d69dbd330b8aa883eadef9dd67771ac
MH-SHUVO20 <mdmehedihasanshuvo994@gmail.com>
2025-09-24 12:11:10 +0600

    Add Grouping in python.py - demonstrate grouping operations and techniques

---
+++ Grouping in python.py
@@ -0,0 +1,25 @@
+numbers=[1,2,3,4,5,6,7,8,9,10]
+print(numbers[2:8])#print(numbers[2:])
+print(numbers[:5])#print number from start to 5
+print(numbers[:])#print all numbers
+print(numbers[::2])#print alternate numbers
+print(numbers[1::2])#print alternate numbers starting from index 1
+print(numbers[::-1])#print numbers in reverse order
+print(numbers[8:2:-1])#print numbers from index 8 to 2
+print(numbers[5:2:-1])#print numbers from index 5 to 2
+print(numbers[5:2])#print empty list because step is +ve
+print(numbers[-1:-6:-1])#print last 5 numbers in reverse order
+print(numbers[-6:-1])#print last 5 numbers in normal order
+print(numbers[-1:])#print last number
+print(numbers[:-6])#print all numbers except last 5 numbers
+print(numbers[-5:])#print last 5 numbers
+print(numbers[2:-3])#print numbers from index 2 to -3
+print(numbers[-3:2:-1])#print numbers from index -3 to 2 in reverse order
+#print even numbers
+print(numbers[1::2])
+#print odd numbers
+print(numbers[0::2])
+#prime numbers
+print([2,3,5,7])
+#divisible by 3
+print([3,6,9])
